64d5cf11d31e167bf239f3e8a398028892f86bb8
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-07-25 12:07:43 -0700

    Fix

--- build.ps1
+++ build.ps1
@@ -1,2 +1,3 @@
 ﻿Remove-Item –path ./client –recurse
 & java -jar swagger-codegen-cli.jar generate -i https://api.cloudmersive.com/swagger/api/convert -l powershell -o client -c packageconfig.json
+& ./client/Build.ps1
