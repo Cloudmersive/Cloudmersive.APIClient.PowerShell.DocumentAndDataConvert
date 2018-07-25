feb9f9006caec6ce2d3c42597f423edd69aadb6f
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-07-25 12:09:27 -0700

    Added

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
 ﻿Remove-Item –path ./client –recurse
 & java -jar swagger-codegen-cli.jar generate -i https://api.cloudmersive.com/swagger/api/convert -l powershell -o client -c packageconfig.json
+Write-Output "Starting post-build";
 & ./client/Build.ps1
